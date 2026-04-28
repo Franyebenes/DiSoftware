2d22aaa55e76b1e518abca8a42847cd0f75c5dbb
FRANCISCO JAVIER YÉBENES CAZALLAS <FcoJavier.Yebenes1@alu.uclm.es>
2026-04-28 10:14:16 +0200

    28/04/2026

--- esientradas/src/main/java/edu/esi/ds/esientradas/EsientradasApplication.java
+++ esientradas/src/main/java/edu/esi/ds/esientradas/EsientradasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EsientradasApplication {
+public class EsientradasApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsientradasApplication.class, args);
