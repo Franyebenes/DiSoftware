067dbe317ad8e155491754f711a68777db71142e
aortega05 <Alejandro.Ortega5@alu.uclm.es>
2026-05-07 22:28:57 +0200

    auth

--- esife/src/app/auth/auth.guard.ts
+++ esife/src/app/auth/auth.guard.ts
@@ -1,23 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core'; // 1. Añadimos Inject y PLATFORM_ID
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common'; // 2. Añadimos isPlatformBrowser
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  // 3. Inyectamos el platformId en el constructor
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object 
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    const token = localStorage.getItem('userToken');
     
-    if (token) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
+    // 4. Envolvemos tu lógica en el 'if' para protegerla del servidor
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('userToken');
+      
+      if (token) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
     }
+
+    // 5. Si es el servidor Node.js, simplemente devolvemos false para que no colapse
+    return false;
   }
 }
